587fcaf0588fe933a498d6eebd35f41f08648179
Hajar0Ahmed <145873045+Hajar0Ahmed@users.noreply.github.com>
2024-04-26 15:57:21 -0500

    Added a helper function to check for valid inputs

--- main.java
+++ main.java
@@ -1,8 +1,19 @@
 //Import Section
 import java.util.Scanner;
 
+/*
+ * Provided in this class is the neccessary code to get started with your game's implementation
+ * You will find a while loop that should take your minefield's gameOver() method as its conditional
+ * Then you will prompt the user with input and manipulate the data as before in project 2
+ *
+ * Things to Note:
+ * 1. Think back to project 1 when we asked our user to give a shape. In this project we will be asking the user to provide a mode. Then create a minefield accordingly
+ * 2. You must implement a way to check if we are playing in debug mode or not.
+ * 3. When working inside your while loop think about what happens each turn. We get input, user our methods, check their return values. repeat.
+ * 4. Once while loop is complete figure out how to determine if the user won or lost. Print appropriate statement.
+ */
 
-public class Main{
+public class main{
     //main, runs through the game
     public static void main(String[] args) {
         //initialize variables
@@ -13,9 +24,9 @@ public class Main{
         boolean debug=false; //True if debug mode selected
         boolean win=false; //True if the player wins
         Scanner scanner = new Scanner(System.in);
-        
-        //create minefield
-        Minefield minefield = null;
+        Minefield minefield = null; //the minefield
+
+
         //ask user for input on mode
         System.out.println("Would you like to play Easy Mode: 1, Medium Mode: 2, or Hard Mode: 3?");
         String mode = scanner.next();
@@ -43,6 +54,7 @@ public class Main{
             mines = 40;
             flags = 40;
         }
+
         //creates minefield with indicated size, flags, and mines
         minefield = new Minefield(rows, cols, flags);
 
@@ -54,7 +66,7 @@ public class Main{
             System.out.println("Not a valid input.");
             System.out.println("Would you like to play in debug mode: yes/no");
             output = scanner.next();
-        //user indicated debug mode
+            //user indicated debug mode
         }if(output.equals("yes")){
             minefield.debug();
             System.out.println(" ");
@@ -65,16 +77,15 @@ public class Main{
             System.out.println(minefield);
             debug=false; //debug set to false
         }
+
         //asks user for initial move
         System.out.println("What is your first move? [x],[y]:");
         String input = scanner.next();
         String[] firstMove = input.split(",");
         //accounts for incorrect user input, input out of bounds
-        while(firstMove.length!=2 || Integer.parseInt(firstMove[0])<0 ||
-                Integer.parseInt(firstMove[0])>rows ||
-                Integer.parseInt(firstMove[1])<0 ||
-                Integer.parseInt(firstMove[1])>cols){
+        while(!checkInput(firstMove,rows, cols, 2)){
             System.out.println("Not a valid input.");
+            scanner.nextLine();//go to next line
             System.out.println("What is your first move? [x],[y] :");
             input = scanner.next();
             firstMove = input.split(",");
@@ -108,11 +119,10 @@ public class Main{
             input = scanner.next();
             String[] move = input.split(",");
             //accounts for incorrect user input, input out of bounds
-            while(move.length!=3 || Integer.parseInt(move[0])<0 || Integer.parseInt(move[0])>rows ||
-                    Integer.parseInt(move[1])<0 || Integer.parseInt(move[1])>cols ||
+            while(!checkInput(move,rows, cols, 3) ||
                     (Integer.parseInt(move[2]) != 0 && Integer.parseInt(move[2]) != 1)){
-
                 System.out.println("Not a valid input.");
+                scanner.nextLine(); //go to next line
                 System.out.println("What is your move? [x] [y] [flag:1 OR reveal:0] ");
                 input = scanner.next();
                 move = input.split(",");
@@ -151,5 +161,29 @@ public class Main{
             //otherwise, user lost
             System.out.println("You Lost!");
         }
+    }
+
+
+    //A helper function: that checks if the input is valid
+    private static boolean checkInput(String[] input, int rows, int cols, int splitNumber) {
+        if (input.length == 0){//if the input does not have a comma it is invalid
+            return false;
+        }
+        //if the input is not a number it is invalid
+        for (int i = 0; i<input.length; i++){
+            try {
+                Integer.parseInt(input[i]);
+            } catch(NumberFormatException e){
+                return false;
+            }
+        }
+        //if the input is out of bounds it is invalid
+        if (input.length!=splitNumber || Integer.parseInt(input[0])<0 ||
+                Integer.parseInt(input[0])>rows ||
+                Integer.parseInt(input[1])<0 ||
+                Integer.parseInt(input[1])>cols)
+            return false;
+        //otherwise it is a valid input
+        return true;
     }
 }
