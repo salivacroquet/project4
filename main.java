5931b571ad0f1d75115fcac2887af9f7c3b4da10
salivacroquet <135278040+salivacroquet@users.noreply.github.com>
2024-04-25 21:31:30 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,11 @@
 //Import Section
-import java.util.Random;
 import java.util.Scanner;
 
 /*
  * Provided in this class is the neccessary code to get started with your game's implementation
  * You will find a while loop that should take your minefield's gameOver() method as its conditional
  * Then you will prompt the user with input and manipulate the data as before in project 2
- * 
+ *
  * Things to Note:
  * 1. Think back to project 1 when we asked our user to give a shape. In this project we will be asking the user to provide a mode. Then create a minefield accordingly
  * 2. You must implement a way to check if we are playing in debug mode or not.
@@ -14,40 +13,40 @@ import java.util.Scanner;
  * 4. Once while loop is complete figure out how to determine if the user won or lost. Print appropriate statement.
  */
 
-public class main(String[] args) {
-    int rows;
-    int cols;
-    int mines;
-    int flags;
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Would you like to play Easy Mode: 1, Medium Mode: 2, or Hard Mode: 3?");
-    int mode = scanner.nextInt();
-    if (mode == 1) {
-        rows = 5;
-        cols = 5;
-        mines = 5;
-        flags = 5;
-        Minefield minefield = new Minefield(rows, cols, flags );
-        //place mines
-        //evaluate
-        //starting area
+public class Main {
+    public static void main(String[] args) {
+        int rows = 0;
+        int cols = 0;
+        int mines = 0;
+        int flags = 0;
+        Scanner scanner = new Scanner(System.in);
+        Minefield minefield = null;
+        System.out.println("Would you like to play Easy Mode: 1, Medium Mode: 2, or Hard Mode: 3?");
+        int mode = scanner.nextInt();
+        //sets size for each mode
+        if (mode == 1) {
+            rows = 5;
+            cols = 5;
+            mines = 5;
+            flags = 5;
+        } else if (mode == 2) {
+            rows = 9;
+            cols = 9;
+            mines = 12;
+            flags = 12;
+        } else if (mode == 3) {
+            rows = 20;
+            cols = 20;
+            mines = 40;
+            flags = 40;
+        } else {
+            System.out.println("Not a valid input.");
+        }
+        minefield = new Minefield(rows, cols, flags);
+
+        while (!minefield.gameOver()) {
+            System.out.println(minefield);
+        }
 
     }
-    if (mode == 2) {
-        rows = 9;
-        cols = 9;
-        mines = 12;
-        flags = 12;
-        Minefield minefield = new Minefield(rows, cols, flags );
-    }
-    if (mode == 3) {
-        rows = 20;
-        cols = 20;
-        mines = 40;
-        flags = 40;
-        Minefield minefield = new Minefield(rows, cols, flags );
-    }
-    while(/* ! minefield.gameOver() */ ){
-    }
-    
 }
